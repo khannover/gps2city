f9509bb015613fe2d3abf017907c1ccaf1510b87
khannover <kai.hannover@gmx.de>
2025-01-28 10:00:55 +0000

    Bugfixing

--- main.py
+++ main.py
@@ -34,7 +34,21 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     distance = R * c
     return distance
 
-def draw_radius(city_data, loc=None):
+def process(latitude, longitude, m, t):
+    with open("german_cities.json") as o:
+        cities = json.load(o)
+        location_candidates = {}
+        for city in cities:
+            location_candidate = draw_radius(city, (latitude, longitude), m)
+            if location_candidate:
+                location_candidates[str(location_candidate["name"])] = location_candidate["distance"]
+
+        # sort candidates by distance
+        sorted_dict = dict(sorted(location_candidates.items(), reverse=True, key=lambda item: item[1]))
+        lc = sorted_dict.popitem() if sorted_dict else ["Unbekannt"]
+        t.text = "Die Geokoordinaten gehören zu " + lc[0]
+
+def draw_radius(city_data, loc=None, map_element=None):
     if not loc:
         loc = (float(input_latitude.value), float(input_longitude.value))
     # 1) Daten parsen
@@ -42,11 +56,11 @@ def draw_radius(city_data, loc=None):
     # Stadtfläche in km²
     city_center_lat = float(city_data["coords"]["lat"])
     city_center_lon = float(city_data["coords"]["lon"])
-
+    
     # 2) Radius aus der Fläche berechnen (Kreisfläche = π * r²)
     radius = math.sqrt(city_area / math.pi)
     color = string_to_hex_color(city_data.get("name"))
-    m.generic_layer(
+    map_element.generic_layer(
         name='circle', 
         args=[
             [city_center_lat, city_center_lon], 
@@ -60,7 +74,7 @@ def draw_radius(city_data, loc=None):
 
     # 3) Entfernung berechnen
     dist = haversine_distance(city_center_lat, city_center_lon, 
-                              loc[0], loc[1])
+                          loc[0], loc[1])
 
     # 5) Vergleich
     if dist <= radius:
@@ -68,50 +82,37 @@ def draw_radius(city_data, loc=None):
         print(f"Distanz zum Stadtzentrum: {dist:.2f} km")
         print(f"Berechneter Radius (Kreisapprox. aus Fläche): {radius:.2f} km")
         city_data["distance"] = dist
-        marker = m.marker(latlng=loc)
+        marker = map_element.marker(latlng=loc)
         return city_data
     else:
         return None
 
-def process():
-    with open("german_cities.json") as o:
-        cities = json.load(o)
-        location_candidates = {}
-        for city in cities:
-            location_candidate = draw_radius(city)
-            if location_candidate:
-                location_candidates[str(location_candidate["name"])] = location_candidate["distance"]
-
-        # sort candidates by distance
-        sorted_dict = dict(sorted(location_candidates.items(), reverse=True, key=lambda item: item[1]))
-        lc = sorted_dict.popitem() if sorted_dict else ["Unbekannt"]
-        label_city.text = "Die Geokoordinaten gehören zu " + lc[0]
 
 
 
+@ui.page("/")
+def index():
 
-with ui.header():
-    ui.label("Germany Geocoordinates").classes("text-2xl")
-    ui.space()
-    ui.link(target="/docs", text="API Documentation").classes("text-white text-2xl")
+    with ui.header():
+        ui.label("Germany Geocoordinates").classes("text-2xl")
+        ui.space()
+        ui.link(target="/docs", text="API Documentation").classes("text-white text-2xl")
 
-with ui.row().classes("w-full items-center"):
-    label_city = ui.label()
+    with ui.row().classes("w-full items-center"):
+        label_city = ui.label()
 
-with ui.row().classes("w-full items-center"):
-    input_latitude = ui.input("Latitude", value=str(52.421936))
-    input_longitude = ui.input("Longitude", value=str(9.696477))
-    ui.button("Find City", on_click=process)
+    with ui.row().classes("w-full items-center") as input_row:
+        input_latitude = ui.input("Latitude", value=str(52.421936))
+        input_longitude = ui.input("Longitude", value=str(9.696477))
 
 
     m = ui.leaflet(center=(51.520, 10.405))
     m.set_zoom(6)
     m.classes("w-full h-[800px]")
+    with input_row:
+        ui.button("Find City", on_click=lambda: process(float(input_latitude.value), float(input_longitude.value), m, label_city))
 
 import math
-
-
-
 
 @app.get("/api/gps2city/{lat}/{lon}")
 def get_city(lat: float, lon: float):
@@ -119,7 +120,7 @@ def get_city(lat: float, lon: float):
         cities = json.load(o)
         location_candidates = {}
         for city in cities:
-            location_candidate = draw_radius(city, (lat, lon))
+            location_candidate = draw_radius(city, (lat, lon), m)
             if location_candidate:
                 location_candidates[str(location_candidate["name"])] = location_candidate["distance"]
         sorted_dict = dict(sorted(location_candidates.items(), reverse=True, key=lambda item: item[0]))
